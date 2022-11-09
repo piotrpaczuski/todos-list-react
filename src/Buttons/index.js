4e1405175b6856fe1263f7e3b861d2935eb1921f
Piotr Paczuski <piotr-paczuski@o2.pl>
2022-11-09 04:29:41 +0100

    corrected class names, added og:image

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,11 +2,11 @@ import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
 
-    <span className="buttons--centred">
+    <span className="buttons">
         {!!tasks.length && (
             <>
-                <button className="buttons">{hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone</button>
-                <button className="buttons"
+                <button className="buttons__button">{hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone</button>
+                <button className="buttons__button"
                     disabled={tasks.every(({ done }) => done)}>Ukończ wszystkie</button>
             </>
         )}
--- src/Section/index.js
+++ src/Section/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Section = ({title, extraContent, body}) => (
     <section className="section">
-        <div className="section__Header section__Header--tasksList">
+        <div className="section__header section__header--tasksList">
             <h2>{title}</h2>
             {extraContent}
         </div>
