eb463785cc224f0ec85ae338ee856979370dd7f4
Piotr Paczuski <piotr-paczuski@o2.pl>
2022-11-30 04:40:13 +0100

    deleted example tasks

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import Header from "./Header";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "Przejść na Reacta", done: false },
-    { id: 2, content: "Zjeść obiad", done: true }
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
